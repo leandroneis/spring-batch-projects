dc27ae44ecfaa7a91fa12371879fe1da509f1fe2
Leandro Neis <lgn200@gmail.com>
2024-11-12 13:42:28 -0300

    Squashed 'PrimeiroProjetoSpringBatchOldVersion/' content from commit 0b31cbd
    git-subtree-dir: PrimeiroProjetoSpringBatchOldVersion
    git-subtree-split: 0b31cbd6886bd54ca8262805d9d815186391a65f

---
+++ src/main/java/com/udemy/primeiroprojetospringbatcholdversion/PrimeiroProjetoSpringBatchOldVersionApplication.java
@@ -0,0 +1,14 @@
+package com.udemy.primeiroprojetospringbatcholdversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringBatchOldVersionApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeiroProjetoSpringBatchOldVersionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/udemy/primeiroprojetospringbatcholdversion/PrimeiroProjetoSpringBatchOldVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.primeiroprojetospringbatcholdversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringBatchOldVersionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
